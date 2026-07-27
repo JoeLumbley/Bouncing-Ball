55e2b1cf1bc437561ca106d19f588f4c11edec5d
Joseph Lumbley <j_lumbley@hotmail.com>
2026-07-27 11:21:07 -0500

    Refactor Form1: remove logic, add resource cleanup
    Removed all previous Form1 logic for ball movement, drawing, and FPS calculation. Added System.ComponentModel and System.Drawing.Drawing2D imports. Replaced Dispose override with Form1_Closing event handler to properly dispose of ballBrush, fpsBrush, and fpsFont resources.

--- Bouncing Ball/Form1.vb
+++ Bouncing Ball/Form1.vb
@@ -21,110 +21,6 @@
 ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ' SOFTWARE.
 
-'Imports System.Drawing.Drawing2D
-
-'Public Class Form1
-
-'    Private ballX As Double
-'    Private ballY As Double
-'    Private ballDiameter As Integer = 80
-
-'    Private velX As Double
-'    Private velY As Double
-'    Private speed As Double = 450 ' pixels per second
-
-'    Private lastUpdate As DateTime = DateTime.Now
-
-'    Private frameCount As Integer = 0
-'    Private fps As Integer = 0
-'    Private lastFpsTime As DateTime = DateTime.Now
-
-'    Private ballBrush As New SolidBrush(Color.DeepSkyBlue)
-'    Private fpsBrush As New SolidBrush(Color.White)
-'    Private fpsFont As New Font("Segoe UI", 14, FontStyle.Bold)
-
-'    Private WithEvents GameTimer As New Timer()
-
-'    Public Sub New()
-'        InitializeComponent()
-
-'        Me.SetStyle(ControlStyles.AllPaintingInWmPaint Or
-'                    ControlStyles.UserPaint Or
-'                    ControlStyles.OptimizedDoubleBuffer, True)
-
-'        Me.BackColor = Color.Black
-
-'        ' Center ball
-'        ballX = (Me.ClientSize.Width - ballDiameter) / 2
-'        ballY = (Me.ClientSize.Height - ballDiameter) / 2
-
-'        ' Pick random direction
-'        Dim rnd As New Random()
-'        Dim angle As Double = rnd.NextDouble() * Math.PI * 2
-
-'        velX = Math.Cos(angle) * speed
-'        velY = Math.Sin(angle) * speed
-
-'        GameTimer.Interval = 1
-'        GameTimer.Start()
-'    End Sub
-
-'    Private Sub GameTimer_Tick(sender As Object, e As EventArgs) Handles GameTimer.Tick
-'        Dim now As DateTime = DateTime.Now
-'        Dim dt As Double = (now - lastUpdate).TotalSeconds
-'        lastUpdate = now
-
-'        ' Move ball
-'        ballX += velX * dt
-'        ballY += velY * dt
-
-'        ' Bounce horizontally
-'        If ballX <= 0 Then
-'            ballX = 0
-'            velX = Math.Abs(velX)
-'        ElseIf ballX >= Me.ClientSize.Width - ballDiameter Then
-'            ballX = Me.ClientSize.Width - ballDiameter
-'            velX = -Math.Abs(velX)
-'        End If
-
-'        ' Bounce vertically
-'        If ballY <= 0 Then
-'            ballY = 0
-'            velY = Math.Abs(velY)
-'        ElseIf ballY >= Me.ClientSize.Height - ballDiameter Then
-'            ballY = Me.ClientSize.Height - ballDiameter
-'            velY = -Math.Abs(velY)
-'        End If
-
-'        Me.Invalidate()
-'    End Sub
-
-'    Protected Overrides Sub OnPaint(e As PaintEventArgs)
-'        MyBase.OnPaint(e)
-
-'        e.Graphics.SmoothingMode = SmoothingMode.AntiAlias
-'        e.Graphics.PixelOffsetMode = PixelOffsetMode.HighQuality
-
-'        ' Draw ball
-'        e.Graphics.FillEllipse(ballBrush, CInt(ballX), CInt(ballY), ballDiameter, ballDiameter)
-
-'        UpdateFPS()
-'        e.Graphics.DrawString($"FPS: {fps}", fpsFont, fpsBrush, 10, 10)
-'    End Sub
-
-'    Private Sub UpdateFPS()
-'        frameCount += 1
-
-'        Dim now As DateTime = DateTime.Now
-'        If (now - lastFpsTime).TotalSeconds >= 1 Then
-'            fps = frameCount
-'            frameCount = 0
-'            lastFpsTime = now
-'        End If
-'    End Sub
-
-'End Class
-
 
 
 Imports System.ComponentModel
@@ -261,15 +157,6 @@ Public Class Form1
     ' -------------------------------
     '  Cleanup
     ' -------------------------------
-
-    'Protected Overrides Sub Dispose(disposing As Boolean)
-    '    If disposing Then
-    '        ballBrush.Dispose()
-    '        fpsBrush.Dispose()
-    '        fpsFont.Dispose()
-    '    End If
-    '    MyBase.Dispose(disposing)
-    'End Sub
 
     Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
 
