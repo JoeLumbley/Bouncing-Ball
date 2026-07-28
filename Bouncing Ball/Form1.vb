c88e2ef690d38f6c9ff67fcfcf3cf2b659121bda
Joseph Lumbley <j_lumbley@hotmail.com>
2026-07-28 15:36:26 -0500

    Add fading trail effect to moving ball
    Added a fading motion trail by storing recent ball positions in a fixed-length list. The trail is rendered as shrinking, semi-transparent ellipses with increasing alpha, creating a smooth fading effect behind the ball.

--- Bouncing Ball/Form1.vb
+++ Bouncing Ball/Form1.vb
@@ -53,6 +53,11 @@ Public Class Form1
     Private fpsBrush As SolidBrush
     Private fpsFont As Font
 
+
+    ' Trail history (stores last N positions)
+    Private trail As New List(Of PointF)
+    Private trailLength As Integer = 25   ' Number of trail segments
+
     Public Sub New()
         InitializeComponent()
 
@@ -123,6 +128,19 @@ Public Class Form1
             velY = -Math.Abs(velY)
         End If
 
+
+
+        ' Record current position for trail
+        trail.Add(New PointF(CSng(ballX), CSng(ballY)))
+
+        ' Keep trail length fixed
+        If trail.Count > trailLength Then
+            trail.RemoveAt(0)
+        End If
+
+
+
+
         Invalidate()
     End Sub
 
@@ -135,6 +153,41 @@ Public Class Form1
         e.Graphics.CompositingMode = CompositingMode.SourceOver
         e.Graphics.SmoothingMode = SmoothingMode.AntiAlias
         e.Graphics.PixelOffsetMode = PixelOffsetMode.HighQuality
+
+
+
+        ' Draw fading trail
+        'Dim alphaStep As Integer = 255 \ trailLength
+        Dim alphaStep As Integer = 32 \ trailLength
+
+        For i As Integer = 0 To trail.Count - 1
+            Dim alpha As Integer = alphaStep * i
+            If alpha > 255 Then alpha = 255
+
+            Using trailBrush As New SolidBrush(Color.FromArgb(alpha, 0, 191, 255)) ' DeepSkyBlue-ish
+                Dim p As PointF = trail(i)
+                Dim size As Integer = ballDiameter - (trailLength - i) * 2 ' Shrinks over time
+                If size < 10 Then size = 10
+
+                'e.Graphics.FillEllipse(trailBrush,
+                '               p.X + (ballDiameter - size) / 2,
+                '               p.Y + (ballDiameter - size) / 2,
+                '               size,
+                '               size)
+
+                e.Graphics.FillEllipse(trailBrush,
+                                       CSng(p.X + (ballDiameter - size) / 2),
+                                       CSng(p.Y + (ballDiameter - size) / 2),
+                                       CSng(size),
+                                       CSng(size))
+
+            End Using
+        Next
+
+
+
+
+
 
         ' Ball
         e.Graphics.FillEllipse(ballBrush,
